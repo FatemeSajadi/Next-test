1c96058841f6bf1f1d055944ddad05304819b196
fateme Sajadi <engineer.sajadi75@gmail.com>
2023-07-20 15:34:57 +0430

    work in bug

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
 import Layout from "../sections/Layout";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import nextI18NextConfig from '../next-i18next.config.js'
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(
+      locale,
+      ['common', 'loginform'],
+      nextI18NextConfig
+    )),
+  },
+})
 
 export default function Home() {
   return (
